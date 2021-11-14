9e047191b0b4c5b1dd0f41debb28633ab223009b
akbr <akbr@akbr.net>
2021-11-13 20:24:27 -0500

    force https on Heroku

--- src/server.ts
+++ src/server.ts
@@ -8,10 +8,24 @@ import { engine } from "./wizard";
 const PORT = process.env.PORT || 5000;
 const distPath = path.resolve("dist/");
 
+//@ts-ignore
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (
+    !req.secure &&
+    req.get("x-forwarded-proto") !== "https" &&
+    process.env.NODE_ENV !== "development"
+  ) {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
 mountRoomServer(
   //@ts-ignore
   express()
     .use(express.static(distPath))
+    .use(requireHTTPS)
     //@ts-ignore
     .get("/", function (_, res) {
       res.sendFile("index.html", { root: distPath });
